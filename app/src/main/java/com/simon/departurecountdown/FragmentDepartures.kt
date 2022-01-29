94cd817b43667abb17b16b488471f7a71427ebde
Šimon Vyhnis <simon.vyhnis@gmail.com>
2022-01-29 23:25:16 +0100

    Initial commit

---
+++ app/src/main/java/com/simon/departurecountdown/FragmentDepartures.kt
@@ -0,0 +1,26 @@
+package com.simon.departurecountdown
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+
+class FragmentDepartures : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+    }
+}
---
+++ app/src/main/java/com/simon/departurecountdown/FragmentStart.kt
@@ -0,0 +1,30 @@
+package com.simon.departurecountdown
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+class FragmentStart : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val input = view.findViewById<EditText>(R.id.editText)
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            if(input.text.toString().isNotEmpty()){
+
+            }
+        }
+    }
+}
